d71c0410e333448e057b2b9577d7a9c2878cb767
Guga Soto <gsoto998@gmail.com>
2023-08-29 09:39:55 -0400

    Subo problema 1 y 2 terminado

--- problema1.py
+++ problema1.py
@@ -1,14 +1,20 @@
-#Problema 1  / 8 ptos x4 pruebas / 32 puntos
-#Concatenación de listas o tuplas
-#--------------------------------
-#Confeccione un programa que lea 2 tuplas sean t1 y t2
-#La salida debe ser una tupla en el orden t2 t1 t2
-#---------------------------------
-#Ejemplo de entrada:
+# Problema 1  / 8 ptos x4 pruebas / 32 puntos
+# Concatenación de listas o tuplas
+# --------------------------------
+# Confeccione un programa que lea 2 tuplas sean t1 y t2
+# La salida debe ser una tupla en el orden t2 t1 t2
+# ---------------------------------
+# Ejemplo de entrada:
 #         20 90 hola
 #		  mundo 44
-#La salida debe ser
+# La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
 t = input().split()
 m = input().split()
-print(m+t+m)
+for i in range(len(t)):
+    if t[i].isdigit():
+        t[i] = int(t[i])
+for i in range(len(m)):
+    if m[i].isdigit():
+        m[i] = int(m[i])
+print(tuple(m) + tuple(t) + tuple(m))
--- problema2.py
+++ problema2.py
@@ -1,11 +1,14 @@
-#Problema 2  / 9 ptos x4 pruebas / 36 puntos
-#Concatenación de listas o tuplas
-#---------------------------------------------------------------------------------
-#Confeccione un programa que lea varios elementos y los entregue de manera inversa
-#---------------------------------------------------------------------------------
-#Ejemplo de entrada:
+# Problema 2  / 9 ptos x4 pruebas / 36 puntos
+# Concatenación de listas o tuplas
+# ---------------------------------------------------------------------------------
+# Confeccione un programa que lea varios elementos y los entregue de manera inversa
+# ---------------------------------------------------------------------------------
+# Ejemplo de entrada:
 #         20 90 hola jiji 77
-#La salida debe ser
+# La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
 t = input().split()
+for i in range(len(t)):
+    if t[i].isdigit():
+        t[i] = int(t[i])
 print(tuple(reversed(t)))
